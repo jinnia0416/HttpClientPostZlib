2ede50ba45a10724036e35bbdcf8ebef86bba4ab
BOBO <48306272+jinnia0416@users.noreply.github.com>
2020-12-13 23:07:17 +0800

    BOBO
    首次簽入
    1.可GET、POST
    2.POST BODY資料可進行zlib壓縮

---
+++ HttpClientRequest/Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace HttpClientRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labHeader = new System.Windows.Forms.Label();
+            this.labHeaderKey = new System.Windows.Forms.Label();
+            this.labHeaderValue = new System.Windows.Forms.Label();
+            this.txbHeadKey1 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal1 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal2 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey2 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal3 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey3 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal4 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey4 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal5 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey5 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal6 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey6 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal10 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey10 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal9 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey9 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal8 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey8 = new System.Windows.Forms.TextBox();
+            this.txbHeadVal7 = new System.Windows.Forms.TextBox();
+            this.txbHeadKey7 = new System.Windows.Forms.TextBox();
+            this.labBody = new System.Windows.Forms.Label();
+            this.txbBody = new System.Windows.Forms.TextBox();
+            this.labUrl = new System.Windows.Forms.Label();
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.comBoxMethod = new System.Windows.Forms.ComboBox();
+            this.comBoxIsZip = new System.Windows.Forms.ComboBox();
+            this.labSendNumber = new System.Windows.Forms.Label();
+            this.txbSendNumber = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labHeader
+            // 
+            this.labHeader.AutoSize = true;
+            this.labHeader.Font = new System.Drawing.Font("標楷體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labHeader.Location = new System.Drawing.Point(26, 74);
+            this.labHeader.Name = "labHeader";
+            this.labHeader.Size = new System.Drawing.Size(55, 13);
+            this.labHeader.TabIndex = 0;
+            this.labHeader.Text = "Header";
+            // 
+            // labHeaderKey
+            // 
+            this.labHeaderKey.AutoSize = true;
+            this.labHeaderKey.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labHeaderKey.Location = new System.Drawing.Point(7, 93);
+            this.labHeaderKey.Name = "labHeaderKey";
+            this.labHeaderKey.Size = new System.Drawing.Size(26, 12);
+            this.labHeaderKey.TabIndex = 1;
+            this.labHeaderKey.Text = "Key";
+            // 
+            // labHeaderValue
+            // 
+            this.labHeaderValue.AutoSize = true;
+            this.labHeaderValue.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labHeaderValue.Location = new System.Drawing.Point(92, 93);
+            this.labHeaderValue.Name = "labHeaderValue";
+            this.labHeaderValue.Size = new System.Drawing.Size(40, 12);
+            this.labHeaderValue.TabIndex = 2;
+            this.labHeaderValue.Text = "Value";
+            // 
+            // txbHeadKey1
+            // 
+            this.txbHeadKey1.Location = new System.Drawing.Point(9, 108);
+            this.txbHeadKey1.Name = "txbHeadKey1";
+            this.txbHeadKey1.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey1.TabIndex = 3;
+            // 
+            // txbHeadVal1
+            // 
+            this.txbHeadVal1.Location = new System.Drawing.Point(94, 108);
+            this.txbHeadVal1.Name = "txbHeadVal1";
+            this.txbHeadVal1.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal1.TabIndex = 4;
+            // 
+            // txbHeadVal2
+            // 
+            this.txbHeadVal2.Location = new System.Drawing.Point(94, 136);
+            this.txbHeadVal2.Name = "txbHeadVal2";
+            this.txbHeadVal2.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal2.TabIndex = 6;
+            // 
+            // txbHeadKey2
+            // 
+            this.txbHeadKey2.Location = new System.Drawing.Point(9, 136);
+            this.txbHeadKey2.Name = "txbHeadKey2";
+            this.txbHeadKey2.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey2.TabIndex = 5;
+            // 
+            // txbHeadVal3
+            // 
+            this.txbHeadVal3.Location = new System.Drawing.Point(94, 164);
+            this.txbHeadVal3.Name = "txbHeadVal3";
+            this.txbHeadVal3.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal3.TabIndex = 8;
+            // 
+            // txbHeadKey3
+            // 
+            this.txbHeadKey3.Location = new System.Drawing.Point(9, 164);
+            this.txbHeadKey3.Name = "txbHeadKey3";
+            this.txbHeadKey3.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey3.TabIndex = 7;
+            // 
+            // txbHeadVal4
+            // 
+            this.txbHeadVal4.Location = new System.Drawing.Point(94, 192);
+            this.txbHeadVal4.Name = "txbHeadVal4";
+            this.txbHeadVal4.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal4.TabIndex = 10;
+            // 
+            // txbHeadKey4
+            // 
+            this.txbHeadKey4.Location = new System.Drawing.Point(9, 192);
+            this.txbHeadKey4.Name = "txbHeadKey4";
+            this.txbHeadKey4.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey4.TabIndex = 9;
+            // 
+            // txbHeadVal5
+            // 
+            this.txbHeadVal5.Location = new System.Drawing.Point(94, 220);
+            this.txbHeadVal5.Name = "txbHeadVal5";
+            this.txbHeadVal5.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal5.TabIndex = 12;
+            // 
+            // txbHeadKey5
+            // 
+            this.txbHeadKey5.Location = new System.Drawing.Point(9, 220);
+            this.txbHeadKey5.Name = "txbHeadKey5";
+            this.txbHeadKey5.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey5.TabIndex = 11;
+            // 
+            // txbHeadVal6
+            // 
+            this.txbHeadVal6.Location = new System.Drawing.Point(94, 248);
+            this.txbHeadVal6.Name = "txbHeadVal6";
+            this.txbHeadVal6.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal6.TabIndex = 14;
+            // 
+            // txbHeadKey6
+            // 
+            this.txbHeadKey6.Location = new System.Drawing.Point(9, 248);
+            this.txbHeadKey6.Name = "txbHeadKey6";
+            this.txbHeadKey6.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey6.TabIndex = 13;
+            // 
+            // txbHeadVal10
+            // 
+            this.txbHeadVal10.Location = new System.Drawing.Point(94, 360);
+            this.txbHeadVal10.Name = "txbHeadVal10";
+            this.txbHeadVal10.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal10.TabIndex = 22;
+            // 
+            // txbHeadKey10
+            // 
+            this.txbHeadKey10.Location = new System.Drawing.Point(9, 360);
+            this.txbHeadKey10.Name = "txbHeadKey10";
+            this.txbHeadKey10.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey10.TabIndex = 21;
+            // 
+            // txbHeadVal9
+            // 
+            this.txbHeadVal9.Location = new System.Drawing.Point(94, 332);
+            this.txbHeadVal9.Name = "txbHeadVal9";
+            this.txbHeadVal9.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal9.TabIndex = 20;
+            // 
+            // txbHeadKey9
+            // 
+            this.txbHeadKey9.Location = new System.Drawing.Point(9, 332);
+            this.txbHeadKey9.Name = "txbHeadKey9";
+            this.txbHeadKey9.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey9.TabIndex = 19;
+            // 
+            // txbHeadVal8
+            // 
+            this.txbHeadVal8.Location = new System.Drawing.Point(94, 304);
+            this.txbHeadVal8.Name = "txbHeadVal8";
+            this.txbHeadVal8.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal8.TabIndex = 18;
+            // 
+            // txbHeadKey8
+            // 
+            this.txbHeadKey8.Location = new System.Drawing.Point(9, 304);
+            this.txbHeadKey8.Name = "txbHeadKey8";
+            this.txbHeadKey8.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey8.TabIndex = 17;
+            // 
+            // txbHeadVal7
+            // 
+            this.txbHeadVal7.Location = new System.Drawing.Point(94, 276);
+            this.txbHeadVal7.Name = "txbHeadVal7";
+            this.txbHeadVal7.Size = new System.Drawing.Size(108, 22);
+            this.txbHeadVal7.TabIndex = 16;
+            // 
+            // txbHeadKey7
+            // 
+            this.txbHeadKey7.Location = new System.Drawing.Point(9, 276);
+            this.txbHeadKey7.Name = "txbHeadKey7";
+            this.txbHeadKey7.Size = new System.Drawing.Size(79, 22);
+            this.txbHeadKey7.TabIndex = 15;
+            // 
+            // labBody
+            // 
+            this.labBody.AutoSize = true;
+            this.labBody.Font = new System.Drawing.Font("標楷體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labBody.Location = new System.Drawing.Point(217, 74);
+            this.labBody.Name = "labBody";
+            this.labBody.Size = new System.Drawing.Size(39, 13);
+            this.labBody.TabIndex = 23;
+            this.labBody.Text = "Body";
+            // 
+            // txbBody
+            // 
+            this.txbBody.Location = new System.Drawing.Point(220, 108);
+            this.txbBody.Multiline = true;
+            this.txbBody.Name = "txbBody";
+            this.txbBody.Size = new System.Drawing.Size(132, 274);
+            this.txbBody.TabIndex = 24;
+            // 
+            // labUrl
+            // 
+            this.labUrl.AutoSize = true;
+            this.labUrl.Font = new System.Drawing.Font("標楷體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labUrl.Location = new System.Drawing.Point(10, 15);
+            this.labUrl.Name = "labUrl";
+            this.labUrl.Size = new System.Drawing.Size(39, 13);
+            this.labUrl.TabIndex = 25;
+            this.labUrl.Text = "URL:";
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(61, 12);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(224, 22);
+            this.txbUrl.TabIndex = 26;
+            this.txbUrl.Text = "http://";
+            // 
+            // comBoxMethod
+            // 
+            this.comBoxMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxMethod.FormattingEnabled = true;
+            this.comBoxMethod.Items.AddRange(new object[] {
+            "GET",
+            "POST"});
+            this.comBoxMethod.Location = new System.Drawing.Point(29, 40);
+            this.comBoxMethod.Name = "comBoxMethod";
+            this.comBoxMethod.Size = new System.Drawing.Size(59, 20);
+            this.comBoxMethod.TabIndex = 27;
+            // 
+            // comBoxIsZip
+            // 
+            this.comBoxIsZip.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxIsZip.FormattingEnabled = true;
+            this.comBoxIsZip.Items.AddRange(new object[] {
+            "內容不壓縮",
+            "內容壓縮"});
+            this.comBoxIsZip.Location = new System.Drawing.Point(94, 40);
+            this.comBoxIsZip.Name = "comBoxIsZip";
+            this.comBoxIsZip.Size = new System.Drawing.Size(94, 20);
+            this.comBoxIsZip.TabIndex = 28;
+            // 
+            // labSendNumber
+            // 
+            this.labSendNumber.AutoSize = true;
+            this.labSendNumber.Location = new System.Drawing.Point(196, 43);
+            this.labSendNumber.Name = "labSendNumber";
+            this.labSendNumber.Size = new System.Drawing.Size(89, 12);
+            this.labSendNumber.TabIndex = 29;
+            this.labSendNumber.Text = "送                    次";
+            // 
+            // txbSendNumber
+            // 
+            this.txbSendNumber.Location = new System.Drawing.Point(214, 38);
+            this.txbSendNumber.Name = "txbSendNumber";
+            this.txbSendNumber.Size = new System.Drawing.Size(52, 22);
+            this.txbSendNumber.TabIndex = 30;
+            this.txbSendNumber.Text = "1";
+            this.txbSendNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(301, 8);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(51, 52);
+            this.btnSend.TabIndex = 31;
+            this.btnSend.Text = "送出";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 392);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txbSendNumber);
+            this.Controls.Add(this.labSendNumber);
+            this.Controls.Add(this.comBoxIsZip);
+            this.Controls.Add(this.comBoxMethod);
+            this.Controls.Add(this.txbUrl);
+            this.Controls.Add(this.labUrl);
+            this.Controls.Add(this.txbBody);
+            this.Controls.Add(this.labBody);
+            this.Controls.Add(this.txbHeadVal10);
+            this.Controls.Add(this.txbHeadKey10);
+            this.Controls.Add(this.txbHeadVal9);
+            this.Controls.Add(this.txbHeadKey9);
+            this.Controls.Add(this.txbHeadVal8);
+            this.Controls.Add(this.txbHeadKey8);
+            this.Controls.Add(this.txbHeadVal7);
+            this.Controls.Add(this.txbHeadKey7);
+            this.Controls.Add(this.txbHeadVal6);
+            this.Controls.Add(this.txbHeadKey6);
+            this.Controls.Add(this.txbHeadVal5);
+            this.Controls.Add(this.txbHeadKey5);
+            this.Controls.Add(this.txbHeadVal4);
+            this.Controls.Add(this.txbHeadKey4);
+            this.Controls.Add(this.txbHeadVal3);
+            this.Controls.Add(this.txbHeadKey3);
+            this.Controls.Add(this.txbHeadVal2);
+            this.Controls.Add(this.txbHeadKey2);
+            this.Controls.Add(this.txbHeadVal1);
+            this.Controls.Add(this.txbHeadKey1);
+            this.Controls.Add(this.labHeaderValue);
+            this.Controls.Add(this.labHeaderKey);
+            this.Controls.Add(this.labHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "API請求";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labHeader;
+        private System.Windows.Forms.Label labHeaderKey;
+        private System.Windows.Forms.Label labHeaderValue;
+        private System.Windows.Forms.TextBox txbHeadKey1;
+        private System.Windows.Forms.TextBox txbHeadVal1;
+        private System.Windows.Forms.TextBox txbHeadVal2;
+        private System.Windows.Forms.TextBox txbHeadKey2;
+        private System.Windows.Forms.TextBox txbHeadVal3;
+        private System.Windows.Forms.TextBox txbHeadKey3;
+        private System.Windows.Forms.TextBox txbHeadVal4;
+        private System.Windows.Forms.TextBox txbHeadKey4;
+        private System.Windows.Forms.TextBox txbHeadVal5;
+        private System.Windows.Forms.TextBox txbHeadKey5;
+        private System.Windows.Forms.TextBox txbHeadVal6;
+        private System.Windows.Forms.TextBox txbHeadKey6;
+        private System.Windows.Forms.TextBox txbHeadVal10;
+        private System.Windows.Forms.TextBox txbHeadKey10;
+        private System.Windows.Forms.TextBox txbHeadVal9;
+        private System.Windows.Forms.TextBox txbHeadKey9;
+        private System.Windows.Forms.TextBox txbHeadVal8;
+        private System.Windows.Forms.TextBox txbHeadKey8;
+        private System.Windows.Forms.TextBox txbHeadVal7;
+        private System.Windows.Forms.TextBox txbHeadKey7;
+        private System.Windows.Forms.Label labBody;
+        private System.Windows.Forms.TextBox txbBody;
+        private System.Windows.Forms.Label labUrl;
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.ComboBox comBoxMethod;
+        private System.Windows.Forms.ComboBox comBoxIsZip;
+        private System.Windows.Forms.Label labSendNumber;
+        private System.Windows.Forms.TextBox txbSendNumber;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ HttpClientRequest/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+using zlib;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HttpClientRequest
+{
+    public partial class Form1 : Form
+    {
+        private static HttpClient hc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //檢查輸入是否正確
+            if (string.IsNullOrWhiteSpace(comBoxMethod.Text) || string.IsNullOrWhiteSpace(comBoxIsZip.Text) ||
+                !Regex.IsMatch(txbSendNumber.Text, "^[0-9]*$") ||
+                (!txbUrl.Text.StartsWith("http://") && !txbUrl.Text.StartsWith("https://")))
+            {
+                MessageBox.Show("請輸入正確的參數!");
+                return;
+            }
+            
+            using (hc)
+            {
+                hc = new HttpClient();
+                //將標頭加入HttpCleint
+                for (int i = 1; i <= 10; i++)
+                {
+                    TextBox key = Controls.Find("txbHeadKey" + i.ToString(), true).FirstOrDefault() as TextBox;
+                    TextBox val = Controls.Find("txbHeadVal" + i.ToString(), true).FirstOrDefault() as TextBox;
+                    if (!string.IsNullOrWhiteSpace(key.Text) && !string.IsNullOrWhiteSpace(val.Text))
+                    {
+                        try{ hc.DefaultRequestHeaders.Add(key.Text, val.Text); }
+                        //不做例外處理
+                        catch { }
+                    }
+                }
+
+                //送出x次
+                int.TryParse(txbSendNumber.Text, out int sendNumber);
+                for (int i = 0; i < sendNumber; i++)
+                {
+                    if (comBoxIsZip.Text == "內容不壓縮")
+                    {
+                        Task.Run(() => SendNotZip(comBoxMethod.Text));
+                    }
+                    else
+                    {
+                        Task.Run(() => SendZip(comBoxMethod.Text));
+                    }
+                }
+
+            }
+        }
+
+        //不壓縮送出
+        private void SendNotZip(string method)
+        {
+            try
+            {
+                if (method == "POST")
+                {
+                    hc.PostAsync(txbUrl.Text, new StringContent(txbBody.Text));
+                }
+                else
+                {
+                    hc.GetAsync(txbUrl.Text);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        //壓縮送出
+        private void SendZip(string method)
+        {
+            try
+            {
+                if (method == "POST")
+                {
+                    //將body壓縮
+                    byte[] bodyBytes = UTF8Encoding.UTF8.GetBytes(txbBody.Text);
+                    MemoryStream ms = new MemoryStream();
+                    Stream bodyZip = new ZOutputStream(ms, zlibConst.Z_STREAM_END);
+                    bodyZip.Write(bodyBytes, 0, bodyBytes.Length);
+                    bodyZip.Close();
+                    byte[] compressData = (byte[])ms.ToArray();
+                    ms.Flush();
+                    ms.Close();
+
+                    //送出請求
+                    hc.PostAsync(txbUrl.Text, new StreamContent(new MemoryStream(compressData)));
+                }
+                else
+                {
+                    hc.GetAsync(txbUrl.Text);
+                }
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
